ffba51a89df54411e079d312954f8e744eed2ecc
alrivasp <alvaro.rivas.p@gmail.com>
2021-01-21 09:29:17 -0300

    First example

---
+++ spec/score_run_spec.rb
@@ -0,0 +1,20 @@
+describe Run do
+
+    describe "attributes" do
+  
+      subject do
+        Run.new(:duration => 32,
+                :distance => 5.2,
+                :timestamp => "2014-12-22 20:30")
+      end
+
+      it { is_expected.to respond_to(:duration) }
+      it "responds to '#duration'" do
+        expect(subject).to respond_to(:duration)
+      end
+
+      it { is_expected.to respond_to(:distance) }
+      it { is_expected.to respond_to(:timestamp) }
+    end
+  end
+
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,27 @@
+require "string_calculator"
+
+describe StringCalculator do
+
+    #describe ".add" do#metodo de clase .add  metodo de instancia #add
+    #    context "given an empty string" do#contexto
+    #        it "returns zero" do#itbloque para describir un ejemplo específico
+    #            expect(StringCalculator.add("")).to eq(0)
+    #        end
+    #    end
+    #end
+
+    describe ".add" do
+        context "given '4'" do
+          it "returns 4" do
+            expect(StringCalculator.add("4")).to eql(4)
+          end
+        end
+    
+        context "given '10'" do
+          it "returns 10" do
+            expect(StringCalculator.add("20")).to eql(10)
+          end
+        end
+      end
+
+end
